d67c5d8afceb259d06df1eea31d41f1e532b37a8
Fabio Ramos Alves <fralves@viajanet.com.br>
2022-03-12 21:51:55 -0300

    incluindo .gitinit

--- src/MyFirstProject/Program.cs
+++ src/MyFirstProject/Program.cs
@@ -6,7 +6,37 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float peso, altura, imc;
+
+            Console.Write("Digite o seu peso:");
+
+            peso = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o seu altura:");
+            altura = float.Parse(Console.ReadLine());
+
+            imc = (peso / (altura * altura));
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Magreza");
+            }
+            if (imc >= 18.5 && imc <= 24.9)
+            {
+                Console.WriteLine("PESO NORMAL");
+            }
+            if (imc >= 25 && imc <= 29.9)
+            {
+                Console.WriteLine("SOBREPESO I");
+            }
+            if (imc >= 30 && imc <= 39.9)
+            {
+                Console.WriteLine("OBESIDADE II");
+            }
+            if (imc >= 40)
+            {
+                Console.WriteLine("OBESIDADE II");
+            }
             Console.ReadLine();
         }
     }
